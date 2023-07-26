520082197013b85ed66d9b2381440be4f1f1105b
Luis Estepa <116852447+LuisEstepa@users.noreply.github.com>
2023-07-26 17:51:22 -0500

    Agregar archivos de proyecto.

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class CategoryEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public string CategoryId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ Entities/ProductEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class ProductEntity
+    {
+        [Key]
+        [StringLength(10)]
+        public string ProductId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ProductName { get; set; }
+
+        [StringLength(600)]
+        public string ProductDescription { get; set; }
+
+        public int TotalQuantity { get; set; }
+
+    }
+}
